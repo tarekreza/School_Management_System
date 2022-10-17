885fb56634f1d1fac9a2995efb80e13bd1cbc5bf
tarekreza <tarekreza15@gmail.com>
2022-10-17 15:37:01 +0600

    some bug fixed

--- app/Models/AccountStudentFee.php
+++ app/Models/AccountStudentFee.php
@@ -37,6 +37,6 @@ class AccountStudentFee extends Model
     }
 
      public function fee_category(){
-    	return $this->belongsTo(FeeCategory::class,'fee_category_id','id');
+    	return $this->belongsTo(feeCategory::class,'fee_category_id','id');
     }
 }
